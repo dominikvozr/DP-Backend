5f8b21548a899d28ae11739167a15be2ebf0d55d
Martin Smetanka <martin.smetanka@gmail.com>
2023-05-03 02:48:48 +0200

    fix: scheduler timezone

--- server/scheduler/scheduler.ts
+++ server/scheduler/scheduler.ts
@@ -111,7 +111,7 @@ class Scheduler {
 
   private async scheduleExamStart(start: Date, jobData: {examId: string}){
     // Define the job schedule to run on a specific date and time (in this example, March 20th 2023 at 12:00:00)
-    schedule.scheduleJob({ start, rule: start, tz: 'Europe/Bratislava' }, function() {
+    schedule.scheduleJob({ start, rule: start, tz: 'Etc/GMT0' }, function() {
       // Find the document you want to update (replace "id_of_exam" with the ID of the exam you want to update)
       Exam.findById(jobData.examId, function(err, exam) {
         if (err) return console.log(err);
@@ -135,7 +135,7 @@ class Scheduler {
   private async scheduleExamEnd(end: Date, jobData: {examId: string}){
     // Define the job schedule to run on a specific date and time (in this example, March 20th 2023 at 12:00:00)
       // Europe/Bratislava
-      schedule.scheduleJob({ start: end, rule: end, tz: 'Europe/Bratislava' }, function() {
+      schedule.scheduleJob({ start: end, rule: end, tz: 'Etc/GMT0' }, function() {
         // Find the document you want to update (replace "id_of_exam" with the ID of the exam you want to update)
         Exam.findById(jobData.examId, function(err, exam) {
           if (err) return console.log(err);
